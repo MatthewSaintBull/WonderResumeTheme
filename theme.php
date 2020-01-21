83c191c1a1f726ded8655b89038b2f58fee194dd
Matteo Santoro <mattinter997@gmail.com>
2020-01-21 22:05:11 +0100

    init theme

---
+++ theme.php
@@ -0,0 +1,33 @@
+<?php global $Wcms ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    	<title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+	<meta name="description" content="<?= $Wcms->page('description') ?>">
+	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css')?>">
+	<?= $Wcms->css()?>
+</head>
+<body>
+	<?= $Wcms->settings() ?>
+	<?= $Wcms->alerts() ?>
+    <?= $Wcms->menu() ?>
+    <h1>
+		<a href="<?= $Wcms->url() ?>">Website home</a>
+	</h1>
+    <?= $Wcms->get('config','siteTitle') ?>
+    <?= $Wcms->page('content') ?>
+    <?= $Wcms->block('subside') ?>
+	<?= $Wcms->footer() ?>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<?= $Wcms->js() ?>
+</body>
+</html>
